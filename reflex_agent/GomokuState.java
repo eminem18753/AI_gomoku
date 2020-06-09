9215533a8b3f62315bc6b4a4cb3dcf5a7199dd4c
unknown <ernie3659656@gmail.com>
2020-06-09 16:10:00 +0800

    initial commit

---
+++ reflex_agent/GomokuState.java
@@ -0,0 +1,14 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class GomokuState
+{
+	public GomokuState()
+	{
+		
+	}
+}
